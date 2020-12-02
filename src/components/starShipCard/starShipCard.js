fd983fbb5e0d0c1db4d16d308e75b85a1ec389b0
jared Long <jlong4223@gmail.com>
2020-12-01 21:58:09 -0600

    adds crew number from state data

--- src/components/starShipCard/starShipCard.js
+++ src/components/starShipCard/starShipCard.js
@@ -3,9 +3,8 @@ import './starShipCard.css'
 export function StarShipCard(props){
     return(
         <div className="StarShipCard">
-           <h1>{props.result.name}</h1> 
-            {/* <h2>Crew Count: {props.result.crew}</h2> */}
-    {/* <h2>Passenger Count: {props.result.passengers}</h2> */}
+           <h1 className="cardtitle">{props.result.name}</h1> 
+            <h2>Crew Count: {props.result.crew}</h2>
         </div>
     )
 }
