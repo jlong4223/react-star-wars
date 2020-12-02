bf73fc551c368d5ec1ecf64e5f705af5af18c792
jared Long <jlong4223@gmail.com>
2020-12-01 20:04:05 -0600

    adds API url and endpoint

---
+++ src/services/sw-api.js
@@ -0,0 +1,7 @@
+const BASE_URL= 'https://swapi.dev/api/'
+
+//get all the starships resources
+
+export function getStarShips(){
+    return fetch(BASE_URL + '/starships/').then(res=> res.json())
+}
